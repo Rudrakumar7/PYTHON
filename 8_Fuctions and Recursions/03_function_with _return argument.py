bf00c2deedf79a16ae93ff0323ced4b4d3771b3f
Rudrakumar7 <rudrakumar7225@gmail.com>
2024-11-25 17:12:13 +0530

    initial  add

---
+++ 8_Fuctions and Recursions/03_function_with _return argument.py
@@ -0,0 +1,10 @@
+
+def goodDay(name, ending):
+    print("Good Day")
+    print(ending)
+    return "ok"
+
+goodDay("Rudra " , "Thank you")
+goodDay("rahul " , "Thanku")
+a = goodDay("roni " , "Thanks")
+print(a)
---
+++ 8_Fuctions and Recursions/11_p6._inch_to_cm.py
@@ -0,0 +1,5 @@
+def inch_to_cm(inch):
+    return inch * 2.54
+
+n = int(input("Enter values in inches : "))
+print("The coressponding value in cm is " , (inch_to_cm(n)))
